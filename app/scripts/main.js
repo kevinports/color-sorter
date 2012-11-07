9d5289c80f97697a40e9243ecef0d9f384d46956
Kevin Ports <kevin.ports@gmail.com>
2012-11-07 00:58:12 -0600

    Added PNG export, drop photo upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
 
 	window.uiDataModel = Backbone.Model.extend({
 		defaults: {
-			image: 'cabins.jpg',
+			image: localStorage.img,
 			imageColor: [],
 			sortMode: 'no-sort',
 			res: 100,
+			remix: 1,
 			cWidth: 0,
 			cHeight: 0
 		}
@@ -24,16 +25,22 @@ $(document).ready(function(){
 	window.bufferCanvasView = Backbone.View.extend({
 		tagName: 'canvas', 
 		className: 'buffer-canvas',
+
 		initialize: function(options){
-			_.bindAll(this, 'render');
+			_.bindAll(this, 'render','updatePhoto');
+			this.vent = options.vent;
+			this.bind('progress', this.progress);
+			this.bind('complete', this.complete);
+			options.vent.bind("updatePhoto", this.updatePhoto);
 		},
 		render: function(){
-			var canvas = this.el,
+			var that = this,
+			canvas = this.el,
 			ctx = canvas.getContext("2d"),
 			model = this.model,
 			width = model.get('cWidth'),
 			height = model.get('cHeight'),
-			url = '../img/'+ model.get('image');
+			url = model.get('image');
 
 			$(canvas).attr({
 				'width': width,
@@ -59,9 +66,13 @@ $(document).ready(function(){
 				}
 
 				model.set({imageColor: colors})	
+
+				that.trigger('complete')
 			}
 
+
 			image.src = url;
+			this.trigger('progress')
 
 			function RGBToHSL(rgb){
 				var r = rgb[0], g = rgb[1], b = rgb[2];
@@ -87,6 +98,18 @@ $(document).ready(function(){
 			}
 
 			return this;
+		},
+		progress: function(){
+			this.vent.trigger("progress", this.model);
+			console.log('progress')
+
+		},
+		complete: function(){
+			this.vent.trigger("complete", this.model);
+			console.log('complete')
+		},
+		updatePhoto: function(){
+			this.render();
 		}
 	});
 
@@ -96,21 +119,29 @@ $(document).ready(function(){
 	template: $('#controls-template').html(),
 	events: {
 	    'mouseup #resolution': 'resolution',
+	    'mouseup #remix': 'remix',
+	    'click #save-image': 'saveImage',
+	    'dragover #dropzone': 'zoneDragover',
+	    'drop #dropzone': 'zoneDrop',
 	    'change #sort-by': 'sortBy'
   	},
     initialize: function(options){
-      _.bindAll(this, 'render','resolution');
+      _.bindAll(this, 'render','resolution','progress','complete','zoneDrop');
+      options.vent.bind("progress", this.progress);
+      options.vent.bind("complete", this.complete);
+      this.bind('updatePhoto', this.updatePhoto);
       this.vent = options.vent;
     },
     render: function(){
 	  	$(this.el).html(Mustache.to_html(this.template, this.model.toJSON()));
 
 	  	var h = $(window).height(),
-					w = $(window).width() - 260;
+			w = $(window).width() - 260;
 			this.model.set({cWidth: w, cHeight: h});
 
-			var bufferCanvas = new bufferCanvasView({model: this.model});
+			var bufferCanvas = new bufferCanvasView({model: this.model, vent: vent});
       $(this.el).append(bufferCanvas.render().el)
+		
 
       return this; 
     },
@@ -123,11 +154,81 @@ $(document).ready(function(){
     	this.model.set({res: resolution});
     	this.vent.trigger('updateUI', this.model);
     },
+    remix: function(evt){
+    	var target = $(evt.currentTarget),
+    	remix = target.val();
+    	this.model.set({remix: remix});
+    	this.vent.trigger('updateUI', this.model);
+    },
+    saveImage: function(){
+    	this.vent.trigger('saveImage', this.model);
+    },
+    zoneDragover:function(event){
+		event.stopPropagation();
+	  	event.preventDefault();
+    },
+    zoneDrop: function(e){
+    	e.stopPropagation();
+		e.preventDefault();
+
+		this.trigger("zoneDrop", e.originalEvent.dataTransfer);
+		var files = e.originalEvent.dataTransfer.files,
+			that = this;
+
+	    // Loop through the FileList and render image files as thumbnails.
+	    for (var i = 0, f; f = files[i]; i++) {
+
+	      // Only process image files.
+	      if (!f.type.match('image.*')) {
+	        continue;
+	      }
+
+	      var reader = new FileReader();
+
+	      // Closure to capture the file information.
+	      reader.onload = (function(theFile) {
+	        return function(e) {
+	          // Render thumbnail.
+	          // var span = document.createElement('span');
+	          // span.html().remove();
+	          // span.innerHTML = ['<img class="thumb" src="', e.target.result,
+	          //                   '" title="', escape(theFile.name), '"/>'].join('');
+
+	          // document.getElementById('list').insertBefore(span, null);
+	          
+	          localStorage.setItem('img', e.target.result);
+	          that.render();
+	          	
+	        };
+	      })(f);
+
+	      // Read in the image file as a data URL.
+	      reader.readAsDataURL(f);
+
+
+	    }
+	    this.trigger('updatePhoto')
+	    this.vent.trigger('updateUI', that.model);
+	    this.render();
+
+    },
+	updatePhoto: function(){
+		this.vent.trigger('updatePhoto', this.model);
+		console.log('update photo')
+	},
     sortBy: function(evt){
     	var target = $(evt.currentTarget),
     		checked = target.find(':checked'),
     		sortMode = checked.attr('value')
     	this.model.set({sortMode: sortMode});
+    	this.vent.trigger('updateUI', this.model);
+    },
+    progress: function(){
+    	$(this.el).find('#thumbnail').addClass('load').prepend('<span class="loadingText">Loading...</span>');
+    },
+    complete: function(){
+    	$(this.el).find('#thumbnail').removeClass('load');
+    	$(this.el).find('.loadingText').addClass('hide');
     	this.vent.trigger('updateUI', this.model);
     }
   });
@@ -136,9 +237,9 @@ $(document).ready(function(){
     tagName: 'canvas', 
     template: $('#canvas-template').html(),
     initialize: function(options){
-      _.bindAll(this, 'render','updateUI'); 
-			options.vent.bind('updateUI', this.updateUI);
-			// this.on('pageLoadUpdate', this.updateUI, this);
+      	_.bindAll(this, 'render','updateUI','saveImage'); 
+		options.vent.bind('updateUI', this.updateUI);
+		options.vent.bind('saveImage', this.saveImage);
     },
 
 	  render: function(){
@@ -154,7 +255,8 @@ $(document).ready(function(){
 						height = data.get('cHeight'),
 						color = data.get('imageColor'),
 						sortMode = data.get('sortMode'),
-						res = data.get('res');
+						res = data.get('res'),
+						remix = data.get('remix');
 
 				$(canvas).attr({
 					'width': width,
@@ -168,6 +270,10 @@ $(document).ready(function(){
 						rectHeight = Math.floor(tempH),
 						colorSort = [];
 
+				// if (remix>1){
+				// 	width = width/(remix*.05);
+				// }
+
 				// get colors from image
 				var i = 0;
 				for (var gridY=0; gridY<tileCount; gridY++) {
@@ -180,7 +286,6 @@ $(document).ready(function(){
 				  }
 				}
 
-				
 				var c;
 
 				switch(sortMode){
@@ -190,21 +295,9 @@ $(document).ready(function(){
 					case 'lightness': c = 2 ; break;
 						
 				}
-				console.log(c)
-
 				colorSort = colorGrid.sort(function (one, other) {
 						   		return other[c] - one[c];
 							});
-
-
-
-
-
-
-
-
-
-
 
 				// draw grid
 				i = 0;
@@ -227,27 +320,38 @@ $(document).ready(function(){
 	  updateUI: function(UIdata){
 	    this.model.set({data: UIdata});
 	    this.render();
+	  },
+	  saveImage: function(){
+	  	var oCanvas = this.el;
+	  	Canvas2Image.saveAsPNG(oCanvas);
 	  }
   });
 
 
 	window.appView = Backbone.View.extend({
 		el: 'body',
-		initialize: function(){
+		initialize: function(options){
+			_.bindAll(this, "progress", "complete");
+			options.vent.bind("progress", this.progress);
+			options.vent.bind("complete", this.complete);
 			this.render();
 		},
-  	render: function(){
+  		render: function(){
 			var controls = new controlView({model: new uiDataModel, vent: vent});
-      $(this.el).append(controls.render().el);
+      		$(this.el).append(controls.render().el);
 
 			var canvas = new canvasView({model: new canvasModel, vent: vent});
-      $(this.el).append(canvas.render().el);
-
-      controls.trigger('pageLoadUpdate');
-    }
+      			$(this.el).append(canvas.render().el);
+    	},
+    	progress: function(){
+    		$(this.el).addClass('loading')
+    	},
+    	complete: function(){
+    		$(this.el).removeClass('loading')
+    	}
 
 	});
 
-  var app = new appView();
+  var app = new appView({vent: vent});
 
 });
